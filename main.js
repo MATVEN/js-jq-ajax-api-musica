c994c0ca5f1ce087536aa128e134a989776e8731
MATVEN <mattiavena2993@gmail.com>
2019-11-19 17:53:26 +0100

    ex complete

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
 $(document).ready(function() {
+
 	//Code
+
+    $.ajax({
+
+      url: "https://flynn.boolean.careers/exercises/api/array/music",
+
+      method: "GET",
+
+      success: function (data) {
+
+        for (var i = 0; i < data.response.length; i++) {
+
+          var source = $("#hb-template").html();
+
+          var sourceTransfert = Handlebars.compile(source);
+
+          var objectsContent = {
+            poster: data.response[i].cover,
+            author: data.response[i].author,
+            year: data.response[i].year,
+            genre: data.response[i].genre
+          }
+
+        var stamp =  sourceTransfert(objectsContent);
+
+          $(".cds-container").append(stamp);
+
+        }
+      },
+
+      error : function (errori) {
+        alert("E' avvenuto un errore. " + errore);
+      }
+
+    })
+
 });
